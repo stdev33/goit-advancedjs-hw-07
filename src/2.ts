1435e767577fcdc7948b8775ab56275bd935387e
Serhii Tarasenko <stdev33@gmail.com>
2023-12-05 19:20:48 +0200

    [*] task 2

--- src/2.ts
+++ src/2.ts
@@ -14,11 +14,13 @@
 
 */
 
+const managerSalarySupplement: number = 10000;
+
 class Employee {
   // Заповніть модифікатори доступу
   name: string;
-  department: string;
-  salary: number;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -33,7 +35,14 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(
+    name: string,
+    department: string,
+    baseSalary: number,
+    salarySupplement: number = managerSalarySupplement
+  ) {
+    super(name, department, baseSalary + salarySupplement);
+  }
 }
-
 
 export {};
